ca4542726fb80b58c0452b586e7578fd389eb7e1
mikecode124 <michaelkimmail@gmail.com>
2024-04-05 12:33:50 -0700

    First push

---
+++ main.py
@@ -0,0 +1,32 @@
+#------------------------------------------------------------------------------
+# Homework 2 - Using Arrays
+# Student: Michael Kim
+# Class: CSSSKL 123
+# Fall 2024
+#
+#
+# In this assignment, we are tasked with reading from a .py file and
+# utilizing/manipulating the data within the input file to perform certain
+# operations while use of loop syntax is or is not restricted. Ultimately,
+# the data values read from will be displayed and compared in visualizations.
+#
+# **Comments that specifies each numbered tasks is in, "Main:", which
+# begins after the import and methods statements sections below.**
+#------------------------------------------------------------------------------
+
+#--> Import module:
+import numpy as np
+
+#--> Methods:
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+#--> Main:
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
