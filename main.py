e08fcbb536d11b18e43b041810b4d0e9808345fc
mikecode124 <michaelkimmail@gmail.com>
2024-04-06 00:56:09 -0700

    added comment blocks and started #1

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
-#------------------------------------------------------------------------------
-# Homework 2 - Using Arrays
-# Student: Michael Kim
-# Class: CSSSKL 123
-# Fall 2024
+# =============================================================================
+# Project:      HW2 - USING ARRAYS
+# Student:      Michael Kim
+# Class:        CSSSKL 123
+# Section:      A, Spring 2024
 #
 #
 # In this assignment, we are tasked with reading from a .py file and
-# utilizing/manipulating the data within the input file to perform certain
-# operations while use of loop syntax is or is not restricted. Ultimately,
-# the data values read from will be displayed and compared in visualizations.
+# utilizing/manipulating the data within 4 separate lists to perform certain
+# operations while use of f/w loop syntax is or is not restricted. Ultimately,
+# the data values read in will be displayed and compared via visualizations.
 #
-# **Comments that specifies each numbered tasks is in, "Main:", which
-# begins after the import and methods statements sections below.**
-#------------------------------------------------------------------------------
+# **Comments that specify each numbered task are in, "Main:", which begins
+# after the import and methods sections below.**
+# =============================================================================
 
-#--> Import module:
+
+# --> Import module:
+
 import numpy as np
 
-#--> Methods:
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+# --> Methods:
+
+def percent_of_mean(file):
+    pass
 
 
-#--> Main:
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# --> Main:
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# -----------------------------------------------------------------------------
+# Task #1:
+# - Loops restricted / List comprehension structures restricted
+# - Write 'percent_of_mean()'.
+#   - Input is a 1-D array of numeric values.
+#       - values represent the stock index for each day as a % of the
+#           mean value over the period covered by the list
+#   - Find mean of all values
+#   - Store representation of each value as a % of the mean
+# -----------------------------------------------------------------------------
+inp_file0 = open("stocks.py")
+
